587fa03fde720de20f85a4cf740a2c86b1fb6fcb
Trần Sỹ Minh Quân <131475958+TheSyx04@users.noreply.github.com>
2026-06-17 15:52:38 +0700

    fix requirements

--- models/init_model.py
+++ models/init_model.py
@@ -1,24 +1,23 @@
-from .deepjit.warper import DeepJIT
-from .tlel.warper import TLELModel
-from .simcom.warper import SimCom
-from .lapredict.warper import LAPredict
-from .lr.warper import LogisticRegression
-from .jitfine.warper import JITFine
-
 models = ["deepjit", "simcom", "lapredict", "tlel", "lr", "jitfine"]
 
 def init_model(model_name, language, device):   
-    if  model_name == "deepjit":
+    if model_name == "deepjit":
+        from .deepjit.warper import DeepJIT
         return DeepJIT(language=language, device=device)
-    elif  model_name == "tlel":
+    elif model_name == "tlel":
+        from .tlel.warper import TLELModel
         return TLELModel(language=language)
     elif model_name == "simcom":
+        from .simcom.warper import SimCom
         return SimCom(language=language, device=device)
     elif model_name == "lapredict":
+        from .lapredict.warper import LAPredict
         return LAPredict(language=language)
     elif model_name == "lr":
+        from .lr.warper import LogisticRegression
         return LogisticRegression(language=language)
     elif model_name == "jitfine":
+        from .jitfine.warper import JITFine
         return JITFine(language=language, device=device)
     else:
         raise Exception("No such model")
